24567f2043611ec465e9d527d40da83d862a709d
ketjow <100692239+Ketjow1410@users.noreply.github.com>
2022-04-12 11:45:59 +0000

    3.2

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ class Main {
   public static void main(String[] args) {
     try {
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      s.addStudent(new Student("Krzysztof", 20,"20.02.2022"));
+      s.addStudent(new Student("Janusz", 40,"20.02.2022"));
 
       
       String imie;
       int wiek;
+      String data;
       int i=0;
       while(i==0){
         System.out.println("Dodanie studenta wybierz 1");
@@ -24,7 +25,9 @@ wybierz =sc.nextInt();
           imie=sc.next();
           System.out.println("Podaj wiek");
           wiek=sc.nextInt();
-          s.addStudent(new Student(imie, wiek));
+          System.out.println("Podaj date urodzin");
+          data=sc.next();
+          s.addStudent(new Student(imie, wiek, data));
           break;
         case 2:{
       var students = s.getStudents();
